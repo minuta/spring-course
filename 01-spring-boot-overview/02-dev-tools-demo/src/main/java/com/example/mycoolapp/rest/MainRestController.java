ce5fc2a6d5e9ca97e458e71b8d7a803c468bb8a1
minuta <gammamustang@gmail.com>
2025-06-22 20:06:07 +0200

    remove spring-boot actuator feature

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/example/mycoolapp/rest/MainRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/example/mycoolapp/rest/MainRestController.java
@@ -1,23 +1,30 @@
 package com.example.mycoolapp.rest;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainRestController {
 
-    @Value("${project.description}")
-    private String projectDescription;
+    // expose "/" that return "Hello World"
 
     @GetMapping("/")
     public String sayHello() {
-        String html = "<h1> Project description : " + projectDescription + "</h1>";
-        return html;
+        return "Hello World!";
     }
+
+    // expose a new endpoint for "workout"
 
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a hard 5k!";
     }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
 }
