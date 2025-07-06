7a255667ad787775e554bec542baaeab4829d068
minuta <gammamustang@gmail.com>
2025-07-06 22:21:27 +0200

    add init state of 02-component-scanning

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/example/springcore_demo/Coach.java
@@ -0,0 +1,7 @@
+package com.example.springcore_demo;
+
+public interface Coach {
+
+    public String getDailyWorkout();
+
+}
---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/example/springcore_demo/SpringcoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcore_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreDemoApplication.class, args);
+	}
+
+}
