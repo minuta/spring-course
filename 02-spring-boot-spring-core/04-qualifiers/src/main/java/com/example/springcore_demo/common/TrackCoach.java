2d2342cacf1274ed9bf6f75756a1553b35f037da
minuta <gammamustang@gmail.com>
2025-07-08 11:08:57 +0200

    finalize 04-qualifiers 2

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/example/springcore_demo/common/TrackCoach.java
@@ -0,0 +1,12 @@
+package com.example.springcore_demo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach {
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // Add any additional methods or properties specific to TrackCoach here
+}
