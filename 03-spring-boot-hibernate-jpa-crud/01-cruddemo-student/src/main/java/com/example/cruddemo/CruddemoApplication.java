aefdcf53563d3c56af3109c6403a34d08443f445
minuta <gammamustang@gmail.com>
2025-08-03 18:21:56 +0200

    add auto creating tables in 01-cruddemo-student

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -21,7 +21,7 @@ public class CruddemoApplication {
 		return runner -> {
 			System.out.println("---> CommandLineRunner is running!");
 
-//			saveStudents(studentDAO);
+			saveStudents(studentDAO);
 
 //			readStudent(studentDAO);
 
@@ -33,7 +33,7 @@ public class CruddemoApplication {
 
 //			deleteStudent(studentDAO);
 
-			deleteAllStudents(studentDAO);
+//			deleteAllStudents(studentDAO);
 		};
 	}
 
