d51b965497c091cbf135344a691d54e62120e451
minuta <gammamustang@gmail.com>
2025-08-23 13:27:53 +0200

    add a new endpoint using a path variable

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -2,8 +2,8 @@ package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +14,10 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    
     private List<Student> students;
 
+    // define @PostConstruct to load the student data
     @PostConstruct
     public void init(){
         students = new ArrayList<>();
@@ -25,10 +27,15 @@ public class StudentRestController {
         students.add(new Student("Jack", "Hardy"));
     }
 
+    // define endpoint for "/students" - return list of students
     @GetMapping("/students")
     public List<Student> getStudents() {
-
-
         return students;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        return students.get(id);
     }
 }
