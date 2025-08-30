23cc4e97842cd5ca909c130da213a8a5ad58895d
minuta <gammamustang@gmail.com>
2025-08-30 16:03:40 +0200

    refactor exception handling to a generic REST exception handler

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -47,31 +47,5 @@ public class StudentRestController {
         return students.get(id);
     }
 
-    // add an Exception Handler using @ExceptionHandler
-    // this will handle StudentNotFoundException
-    // and return a custom 404 error response
-    // the exception handler will return a ResponseEntity
-    // containing a StudentErrorResponse and the status code 404
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleResponse(StudentNotFoundException ex) {
 
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setMessage(ex.getMessage());
-        errorResponse.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleResponse(Exception ex) {
-
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setMessage(ex.getMessage());
-        errorResponse.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
 }
